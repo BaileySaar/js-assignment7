96245bd93b607b1972fa8b7d0a537e1b31a3aade
Bailey Saario <300090184@fvtc.edu>
2024-04-15 15:53:50 -0500

    Completed the assignment

--- app.js
+++ app.js
@@ -20,11 +20,25 @@ app.get('/', (_, response) => {
 
 
 // GET /api/todos
-
+app.get('/api/todos', (request, response) => {
+    console.log('Fetching todos.')
+    response.json(todos)
+})
 // POST /api/todos
+app.post('/api/todos',  (request, response) => {
+    console.log('Posting todo.')
+    const { id, item, complete } = request.body
+    todos.push({ id: todos.length+1, item, complete: false })
+    response.json({ message: 'New todo added.' })
+})
 
 // PUT /api/todos/:id
-
+app.put('/api/todos/:id', (request, response) => {
+	console.log('Put todo.')
+	const { id } = request.params
+	const task = todos.find(todo => todo.id.toString() === id)
+	task.complete = !task.complete // toggle the complete property
+})
 
 
 const message = `Server running: http://localhost:${port}`
